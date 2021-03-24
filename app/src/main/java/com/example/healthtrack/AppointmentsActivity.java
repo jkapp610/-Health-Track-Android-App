59adea11c2b204e2215848d93b6bb0b6997a550e
Alicia Wagner <alicia.wagner@cyprx.com>
2021-03-24 15:07:41 -0400

    Can swap between main and appt screens.

--- app/src/main/java/com/example/healthtrack/AppointmentsActivity.java
+++ app/src/main/java/com/example/healthtrack/AppointmentsActivity.java
@@ -6,17 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class AppointmentsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_appointments);
     }
 
-    public void onClickAppointments(View view){
-        Intent intent = new Intent(this, AppointmentsActivity.class);
+    public void onClickHome(View view){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
 }
