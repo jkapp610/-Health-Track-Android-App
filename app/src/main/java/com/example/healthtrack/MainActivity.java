22f08f4d057670e73b8af7143aa15f9d9dea0408
Alicia Wagner <alicia.wagner@cyprx.com>
2021-03-24 17:03:55 -0400

    Added profile name on main screen.
    Currently it is just a hardcoded string, so we will have connect it to the database later.

--- app/src/main/java/com/example/healthtrack/MainActivity.java
+++ app/src/main/java/com/example/healthtrack/MainActivity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView nameTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameTxt = (TextView) findViewById(R.id.nameTxt);
+        nameTxt.setText("Billy Bob Joe"); //TODO get patient name from database
     }
 
     public void onClickAppointments(View view){
