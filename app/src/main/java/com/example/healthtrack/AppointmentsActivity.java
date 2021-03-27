8a0525f63c18039e291bcd0931b2f829aea1f655
Alicia Wagner <alicia.wagner@cyprx.com>
2021-03-27 17:20:59 -0400

    Appointments scrolling completed!

--- app/src/main/java/com/example/healthtrack/AppointmentsActivity.java
+++ app/src/main/java/com/example/healthtrack/AppointmentsActivity.java
@@ -5,25 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ScrollView;
-import android.widget.SimpleAdapter;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class AppointmentsActivity extends AppCompatActivity {
 
-    ScrollView appointmentsView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointments);
-        appointmentsView = findViewById(R.id.appointmentsView);
-        FillList();
+
+        fillAppointments();
     }
 
     public void onClickHome(View view){
@@ -32,31 +28,6 @@ public class AppointmentsActivity extends AppCompatActivity {
     }
 
     // Just a test for now... //TODO get actual data from the database
-    public void FillList() {
-        try {
-
-            Appointment items = new Appointment();
-            List<Map<String, String>> prolist = new ArrayList<Map<String, String>>();
-            String[] from = {"ItemRef", "Item", "Supplier", "LatestStock", "SalePrice", "BinNumber", "Balance"};
-            int[] views = {R.id.txtPractioner, R.id.txtDate};
-
-            List<Appointment> lsr = items.GetItems();
-
-            for (int i = 0; i < lsr.size(); i++) {
-                Map<String, String> datanum = new HashMap<String, String>();
-                datanum.put("Practitioner", lsr.get(i).Item);
-                datanum.put("Date", lsr.get(i).Item);
-
-                prolist.add(datanum);
-            }
-            // TODO create "lst_items.xml" to make the formating template for the list
-            final SimpleAdapter simpleAdapter = new SimpleAdapter(AppointmentsActivity.this, prolist, R.layout.lst_items, from, views);
-            appointmentsView.setAdapter(simpleAdapter);
-        } catch (Exception ex) {
-            Toast.makeText(AppointmentsActivity.this, ex.getMessage().toString(),Toast.LENGTH_LONG).show();
-        }
-    }
-
     public class Appointment {
         private String practitoner;
         private String date;
@@ -73,12 +44,50 @@ public class AppointmentsActivity extends AppCompatActivity {
         public List<Appointment> GetItems() {
             List<Appointment> lstItems = new ArrayList<Appointment>();
 
-            lstItems.add(new Appointment("Dr. Hilton","9:30AM Feb. 28th 2021");
-            lstItems.add(new Appointment("Dr. Sparks","11:00AM Aug. 10th 2021");
-            lstItems.add(new Appointment("Dr. Allison","10:00AM May. 4th 2021");
+            lstItems.add(new Appointment("Dr. Hilton","9:30AM Feb. 28th 2021"));
+            lstItems.add(new Appointment("Dr. Sparks","11:00AM Aug. 10th 2021"));
+            lstItems.add(new Appointment("Dr. Allison","10:00AM May. 4th 2021"));
+            lstItems.add(new Appointment("Dr. Varde","2:00PM Sep. 24th 2021"));
 
             return lstItems;
         }
+    }
+
+    // Just a test for now... //TODO get actual data from the database
+    private void fillAppointments(){
+        Appointment appointment = new Appointment();
+        List<Appointment> appt = appointment.GetItems();
+        int numAppt = appt.size();
+
+        Button btn0 = (Button) findViewById(R.id.apt0);
+        if(numAppt > 0) {
+            btn0.setText(appt.get(0).practitoner + "\n" + appt.get(0).date);
+            btn0.setVisibility(View.VISIBLE);
+        }else btn0.setVisibility(View.INVISIBLE);
+
+        Button btn1 = (Button) findViewById(R.id.apt1);
+        if(numAppt > 1) {
+            btn1.setText(appt.get(1).practitoner + "\n" + appt.get(1).date);
+            btn1.setVisibility(View.VISIBLE);
+        }else btn1.setVisibility(View.INVISIBLE);
+
+        Button btn2 = (Button) findViewById(R.id.apt2);
+        if(numAppt > 2) {
+            btn2.setText(appt.get(2).practitoner + "\n" + appt.get(2).date);
+            btn2.setVisibility(View.VISIBLE);
+        }else btn2.setVisibility(View.INVISIBLE);
+
+        Button btn3 = (Button) findViewById(R.id.apt3);
+        if(numAppt > 3) {
+            btn3.setText(appt.get(3).practitoner + "\n" + appt.get(3).date);
+            btn3.setVisibility(View.VISIBLE);
+        }else btn3.setVisibility(View.INVISIBLE);
+
+        Button btn4 = (Button) findViewById(R.id.apt4);
+        if(numAppt > 4) {
+            btn4.setText(appt.get(4).practitoner + "\n" + appt.get(4).date);
+            btn4.setVisibility(View.VISIBLE);
+        }else btn4.setVisibility(View.INVISIBLE);
     }
 }
 
