ff7c6ed22839ec178ee061f6aeb76e6b55ece698
Alicia Wagner <alicia.wagner@cyprx.com>
2021-03-27 20:18:19 -0400

    Create appointment screen in progress.
    - drop down menu is working

--- app/src/main/java/com/example/healthtrack/NewAppointmentActivity.java
+++ app/src/main/java/com/example/healthtrack/NewAppointmentActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class NewAppointmentActivity extends AppCompatActivity {
 
@@ -12,10 +14,19 @@ public class NewAppointmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_appointment);
+
+        //TODO actually get list of practitioners from the database!
+        String[] practitioners = { "Dr. Hilton", "Dr. Sparks", "Dr. Allison", "Dr. Varde" };
+        Spinner spin = (Spinner) findViewById(R.id.practitionerList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item, practitioners);
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        spin.setAdapter(adapter);
     }
 
     public void onClickHome(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+
 }
