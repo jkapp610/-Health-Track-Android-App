c809e5d08f5a8105fc8938198d5d701b0979cd6b
Alicia Wagner <alicia.wagner@cyprx.com>
2021-04-03 13:51:46 -0400

    Appointment screen in progress.
    - pre-populating values in edit screen
    - added "time" & "reminder" values to appointment class (for temp testing)
    - changed appointments to display "time" below "date"

--- app/src/main/java/com/example/healthtrack/AppointmentsActivity.java
+++ app/src/main/java/com/example/healthtrack/AppointmentsActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ScrollView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,34 +32,67 @@ public class AppointmentsActivity extends AppCompatActivity {
     }
 
     public void onClickEdit(View view){
-        //TODO: init values for the specific appt we clicked on, so we can pre-populate the fields in
-        // the next screen. We're already grabbing the data & we know which button was pressed, so
-        // this is the easiest way to get the existing appt info.
+        //TODO: get actual data from the database.
+        //NOTE: there is probably a much better way of doing this...
+        // Pre-populate the fields on the following edit screen.
         Intent intent = new Intent(this, EditAppointmentActivity.class);
+        Button btn = (Button) view;
+        AppointmentsActivity.Appointment appointment = new AppointmentsActivity.Appointment();
+        List<AppointmentsActivity.Appointment> appt = appointment.GetItems();
+        if( (Button) findViewById(R.id.appt0) == btn) {
+            intent.putExtra("key_practitioner", appt.get(0).practitioner);
+            intent.putExtra("key_date", appt.get(0).date);
+            intent.putExtra("key_time", appt.get(0).time);
+            intent.putExtra("key_reminder", appt.get(0).reminder);
+        }else if( (Button) findViewById(R.id.appt1) == btn){
+            intent.putExtra("key_practitioner", appt.get(1).practitioner);
+            intent.putExtra("key_date", appt.get(1).date);
+            intent.putExtra("key_time", appt.get(1).time);
+            intent.putExtra("key_reminder", appt.get(1).reminder);
+        }else if( (Button) findViewById(R.id.appt2) == btn){
+            intent.putExtra("key_practitioner", appt.get(2).practitioner);
+            intent.putExtra("key_date", appt.get(2).date);
+            intent.putExtra("key_time", appt.get(2).time);
+            intent.putExtra("key_reminder", appt.get(2).reminder);
+        }else if( (Button) findViewById(R.id.appt3) == btn){
+            intent.putExtra("key_practitioner", appt.get(3).practitioner);
+            intent.putExtra("key_date", appt.get(3).date);
+            intent.putExtra("key_time", appt.get(3).time);
+            intent.putExtra("key_reminder", appt.get(3).reminder);
+        }else if( (Button) findViewById(R.id.appt4) == btn){
+            intent.putExtra("key_practitioner", appt.get(4).practitioner);
+            intent.putExtra("key_date", appt.get(4).date);
+            intent.putExtra("key_time", appt.get(4).time);
+            intent.putExtra("key_reminder", appt.get(4).reminder);
+        }
         startActivity(intent);
     }
 
     // Just a test for now... //TODO get actual data from the database
     public class Appointment {
-        private String practitoner;
+        private String practitioner;
         private String date;
+        private String time;
+        private Boolean reminder;
 
         public Appointment() {
 
         }
 
-        public Appointment(String practitoner, String date) {
-            this.practitoner = practitoner;
+        public Appointment(String practitioner, String date,String time,Boolean reminder) {
+            this.practitioner = practitioner;
             this.date = date;
+            this.time = time;
+            this.reminder = reminder;
         }
 
         public List<Appointment> GetItems() {
             List<Appointment> lstItems = new ArrayList<Appointment>();
 
-            lstItems.add(new Appointment("Dr. Hilton","9:30AM Feb. 28th 2021"));
-            lstItems.add(new Appointment("Dr. Sparks","11:00AM Aug. 10th 2021"));
-            lstItems.add(new Appointment("Dr. Allison","10:00AM May. 4th 2021"));
-            lstItems.add(new Appointment("Dr. Varde","2:00PM Sep. 24th 2021"));
+            lstItems.add(new Appointment("Dr. Hilton","Feb. 28th 2021","9:30AM ",true));
+            lstItems.add(new Appointment("Dr. Sparks","Aug. 10th 2021","11:00AM ",false));
+            lstItems.add(new Appointment("Dr. Allison","May. 4th 2021","10:00AM ",true));
+            lstItems.add(new Appointment("Dr. Varde","Sep. 24th 2021","2:00PM",false));
 
             return lstItems;
         }
@@ -72,33 +104,33 @@ public class AppointmentsActivity extends AppCompatActivity {
         List<Appointment> appt = appointment.GetItems();
         int numAppt = appt.size();
 
-        Button btn0 = (Button) findViewById(R.id.apt0);
+        Button btn0 = (Button) findViewById(R.id.appt0);
         if(numAppt > 0) {
-            btn0.setText(appt.get(0).practitoner + "\n" + appt.get(0).date);
+            btn0.setText(appt.get(0).practitioner + "\n" + appt.get(0).date + "\n" + appt.get(0).time);
             btn0.setVisibility(View.VISIBLE);
         }else btn0.setVisibility(View.INVISIBLE);
 
-        Button btn1 = (Button) findViewById(R.id.apt1);
+        Button btn1 = (Button) findViewById(R.id.appt1);
         if(numAppt > 1) {
-            btn1.setText(appt.get(1).practitoner + "\n" + appt.get(1).date);
+            btn1.setText(appt.get(1).practitioner + "\n" + appt.get(1).date + "\n" + appt.get(1).time);
             btn1.setVisibility(View.VISIBLE);
         }else btn1.setVisibility(View.INVISIBLE);
 
-        Button btn2 = (Button) findViewById(R.id.apt2);
+        Button btn2 = (Button) findViewById(R.id.appt2);
         if(numAppt > 2) {
-            btn2.setText(appt.get(2).practitoner + "\n" + appt.get(2).date);
+            btn2.setText(appt.get(2).practitioner + "\n" + appt.get(2).date + "\n" + appt.get(2).time);
             btn2.setVisibility(View.VISIBLE);
         }else btn2.setVisibility(View.INVISIBLE);
 
-        Button btn3 = (Button) findViewById(R.id.apt3);
+        Button btn3 = (Button) findViewById(R.id.appt3);
         if(numAppt > 3) {
-            btn3.setText(appt.get(3).practitoner + "\n" + appt.get(3).date);
+            btn3.setText(appt.get(3).practitioner + "\n" + appt.get(3).date + "\n" + appt.get(3).time);
             btn3.setVisibility(View.VISIBLE);
         }else btn3.setVisibility(View.INVISIBLE);
 
-        Button btn4 = (Button) findViewById(R.id.apt4);
+        Button btn4 = (Button) findViewById(R.id.appt4);
         if(numAppt > 4) {
-            btn4.setText(appt.get(4).practitoner + "\n" + appt.get(4).date);
+            btn4.setText(appt.get(4).practitioner + "\n" + appt.get(4).date + "\n" + appt.get(4).time);
             btn4.setVisibility(View.VISIBLE);
         }else btn4.setVisibility(View.INVISIBLE);
     }
--- app/src/main/java/com/example/healthtrack/EditAppointmentActivity.java
+++ app/src/main/java/com/example/healthtrack/EditAppointmentActivity.java
@@ -1,10 +1,13 @@
 package com.example.healthtrack;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +24,20 @@ public class EditAppointmentActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item, practitioners);
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
         spin.setAdapter(adapter);
+
+        // Pre-populated the fields from the previous screen based on pressed button.
+        Intent intent = getIntent();
+        String practitioner = intent.getStringExtra("key_practitioner");
+        String date = intent.getStringExtra("key_date");
+        String time = intent.getStringExtra("key_time");
+        Boolean reminder = intent.getBooleanExtra("key_reminder",false);
+        for(int i = 0; i < adapter.getCount(); i++) if(practitioner.equals(adapter.getItem(i))) spin.setSelection(i);
+        EditText dateTxt = findViewById(R.id.dateTxt);
+        dateTxt.setText(date);
+        EditText timeTxt = findViewById(R.id.timeTxt);
+        timeTxt.setText(time);
+        Switch remindSwitch = findViewById(R.id.remindSwitch);
+        remindSwitch.setChecked(reminder);
     }
 
     public void onClickHome(View view){
