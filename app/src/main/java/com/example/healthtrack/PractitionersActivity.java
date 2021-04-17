2469cd2b717d72b8cf6548c6c4b04cc2388e70be
jkapp610 <jkapp@umich.edu>
2021-04-16 20:15:00 -0400

    can now display Practitioners for given user

--- app/src/main/java/com/example/healthtrack/PractitionersActivity.java
+++ app/src/main/java/com/example/healthtrack/PractitionersActivity.java
@@ -1,21 +1,34 @@
 package com.example.healthtrack;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PractitionersActivity extends AppCompatActivity {
+    private  static final String TAG = "PractitionerActivity";
+    private  int pract;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practitioners);
+        Log.d(TAG, "I am here in on create");
 
         fillPractitioners();
     }
@@ -30,7 +43,7 @@ public class PractitionersActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void onClickEdit(View view){
+    /*public void onClickEdit(View view){
         //TODO: get actual data from the database.
         //NOTE: there is probably a much better way of doing this...
         // Pre-populate the fields on the following edit screen.
@@ -60,10 +73,10 @@ public class PractitionersActivity extends AppCompatActivity {
             intent.putExtra("key_address",pract.get(4).address);
         }
         startActivity(intent);
-    }
+    }*/
 
     // Just a test for now... //TODO get actual data from the database
-    public class Practitioner {
+    /*public class Practitioner {
         private String name;
         private String title;
         private String address;
@@ -88,11 +101,65 @@ public class PractitionersActivity extends AppCompatActivity {
 
             return lstItems;
         }
-    }
+    }*/
 
     // Just a test for now... //TODO get actual data from the database
     private void fillPractitioners(){
-        PractitionersActivity.Practitioner practitioner = new PractitionersActivity.Practitioner();
+        FirebaseAuth myAuth = FirebaseAuth.getInstance();
+        String currentuserID = myAuth.getCurrentUser().getUid();
+        FirebaseDatabase mydatabase = FirebaseDatabase.getInstance();
+        DatabaseReference myref = mydatabase.getReference("Patients Practitoner");
+
+        Button btn0 = (Button) findViewById(R.id.pract0);
+        Button btn1 = (Button) findViewById(R.id.pract1);
+        Button btn2 = (Button) findViewById(R.id.pract2);
+        Button btn3 = (Button) findViewById(R.id.pract3);
+        Button btn4 = (Button) findViewById(R.id.pract4);
+
+
+        //btn0.setVisibility(View.INVISIBLE);
+        myref.child(currentuserID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.d(TAG, "I am here");
+                for (DataSnapshot ds: snapshot.getChildren()){
+                     String mykey = ds.getKey();
+                        Log.d(TAG,"The key is "+mykey);
+                        if(pract ==0){
+                            getpractitioner(mykey,btn0);
+                        }
+                        if (pract ==1){
+                            getpractitioner(mykey,btn1);
+
+                        }
+                         if (pract ==2){
+                            getpractitioner(mykey,btn2);
+
+                        }
+                        if (pract ==3){
+                            getpractitioner(mykey,btn3);
+
+                        }
+                    if (pract ==4){
+                        getpractitioner(mykey,btn4);
+
+                    }
+
+
+
+                       pract = pract+1;
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        /*PractitionersActivity.Practitioner practitioner = new PractitionersActivity.Practitioner();
         List<PractitionersActivity.Practitioner> pract = practitioner.GetItems();
         int numPract = pract.size();
 
@@ -124,6 +191,27 @@ public class PractitionersActivity extends AppCompatActivity {
         if(numPract > 4) {
             btn4.setText(pract.get(4).name + "\n" + pract.get(4).title + "\n" + pract.get(4).address);
             btn4.setVisibility(View.VISIBLE);
-        }else btn4.setVisibility(View.INVISIBLE);
+        }else btn4.setVisibility(View.INVISIBLE);*/
     }
+    void getpractitioner(String mykey,Button button ){
+        FirebaseDatabase mydatabase = FirebaseDatabase.getInstance();
+        DatabaseReference myref2 = mydatabase.getReference("Practitioner");
+        myref2.child(mykey).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Practitioner userdoc = snapshot.getValue(Practitioner.class);
+                button.setText(userdoc.getDoctorname()+"\n"+userdoc.getTitle()+"\n"+userdoc.getAddress());
+                Log.d(TAG,userdoc.getDoctorname()+"\n"+userdoc.getTitle()+"\n"+userdoc.getAddress());
+                button.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d(TAG,"Something went Wrong");
+
+            }
+        });
+
+    }
+
 }
