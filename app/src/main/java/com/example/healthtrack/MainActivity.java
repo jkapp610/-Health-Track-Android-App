2f7bb1a5590b33f9768b0b3fa11b3f1bf9052153
stephros18 <60150941+stephros18@users.noreply.github.com>
2021-04-19 21:52:15 -0400

    Added Notification Code

--- app/src/main/java/com/example/healthtrack/MainActivity.java
+++ app/src/main/java/com/example/healthtrack/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.healthtrack;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView nameTxt;
+    Button notifyBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         nameTxt = (TextView) findViewById(R.id.nameTxt);
         nameTxt.setText("Billy Bob Joe"); //TODO get patient name from database
+
+        notifyBtn = findViewById(R.id.notify_btn);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("My Notification", "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+
+        }
+
+        notifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //notification goes here
+                String msgText = "You have an appointment with Dr. Hilton "
+                        + "Feb. 28th 2021 at "
+                        + "9:30 AM";
+
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"My Notification");
+                builder.setContentTitle("Appointments");
+                builder.setStyle(new NotificationCompat.BigTextStyle().bigText(msgText));
+                builder.setContentText(msgText).build();
+                builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+                managerCompat.notify(1,builder.build());
+
+               /* NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"My Notification");
+                builder.setContentTitle("Appointments");
+                builder.setContentText("Dr. Hilton; Feb. 28th 2021 @ 9:30 AM");
+                builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+                //mBuilder.setSmallIcon(R.mipmap.ic_launcher_round);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+                managerCompat.notify(1,builder.build());*/
+
+            }
+        });
+
     }
 
     public void onClickProfile(View view){
